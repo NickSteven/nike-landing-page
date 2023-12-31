d616583ee86f03e20d9bd11d23b1dc4ef820fc3e
Nick Steven <nickandoniaina@gmaim.com>
2023-12-31 14:19:55 +0300

    setting cache busting

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -12,7 +12,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
             onClick={handleClick}
         >
             <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
-                <img src={imgURL.thumbnail}
+                <img src={`${imgURL.thumbnail}?${new Date().getTime()}`}
                     alt="shoe collection"
                     width={127}
                     height={103}
